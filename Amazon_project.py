8f69ba009a38a9656d0b4d376684dd9a32665539
Mohsina-Ahmed <122023111+Mohsina-Ahmed@users.noreply.github.com>
2023-10-21 22:28:06 +0100

    Update Amazon_project.py

--- Amazon_project.py
+++ Amazon_project.py
@@ -30,7 +30,7 @@ print(price_as_float)
 #if price_as_float < 200:
 if price_as_float < 140:
     my_gmail = "myworkuse93@gmail.com"
-    password = "{{ github.gmail_secret }}"
+    password = "${{ secrets.GMAIL_SECRET }}"
     connection = smtplib.SMTP("smtp.gmail.com")
     connection.starttls()  # make connection secure
     connection.login(user=my_gmail, password=password)
